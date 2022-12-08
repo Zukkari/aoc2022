304df06ddd91373b0ce3ad3679e5b553e1606938
zukkari <stanislav.myshkovski@gmail.com>
2022-12-08 18:04:13 +0200

    Day 8 patial

---
+++ lib/day8.ml
@@ -0,0 +1,62 @@
+open Base
+open Stdio
+
+type row = int list [@@deriving show]
+type grid = row list
+type pos = int * int [@@deriving show]
+
+module Visibility = struct
+  let from_right grid (r, c) =
+    let row = List.nth_exn grid r in
+    let target = List.nth_exn row c in
+    let from_edge = List.sub ~pos:0 ~len:c row in
+    List.for_all ~f:(fun tree -> tree < target) from_edge
+
+  let from_top grid (r, c) =
+    let row = List.nth_exn grid r in
+    let target = List.nth_exn row c in
+    let from_edge =
+      List.map ~f:(fun row -> List.nth_exn row c) grid |> fun trees ->
+      List.take trees r
+    in
+    List.for_all ~f:(fun tree -> tree < target) from_edge
+
+  let from_left grid (r, c) =
+    let row = List.nth_exn grid r in
+    let target = List.nth_exn row c in
+    let from_edge = List.sub ~pos:c ~len:(List.length row - c) row in
+    List.for_all ~f:(fun tree -> tree < target) from_edge
+
+  let from_bottom grid (r, c) =
+    let row = List.nth_exn grid r in
+    let target = List.nth_exn row c in
+    let from_edge =
+      List.map ~f:(fun row -> List.nth_exn row c) grid |> fun trees ->
+      List.drop trees (r + 1)
+    in
+    List.for_all ~f:(fun tree -> tree < target) from_edge
+end
+
+module Tree = struct
+  let is_visible grid pos =
+    let () = print_endline (show_pos pos) in
+    Visibility.from_left grid pos
+    || Visibility.from_top grid pos
+    || Visibility.from_right grid pos
+    || Visibility.from_bottom grid pos
+end
+
+let read_lines ~file ~f =
+  In_channel.with_file file ~f:(fun inc ->
+      In_channel.input_lines inc |> List.map ~f)
+
+let to_int_list line =
+  String.to_list line |> List.map ~f:String.of_char |> List.map ~f:Int.of_string
+
+let solve_p1 file =
+  let grid = read_lines ~file ~f:to_int_list in
+  List.mapi
+    ~f:(fun x row ->
+      List.mapi ~f:(fun y _ -> if Tree.is_visible grid (x, y) then 1 else 0) row)
+    grid
+  |> List.fold ~init:0 ~f:(fun acc row -> acc + List.fold ~init:0 ~f:( + ) row)
